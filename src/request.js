bed80250215c97aa5d28706fe9eaf69ba1265574
Emil Wall <emil.wall@valtech.se>
2013-05-13 14:21:25 +0200

    Accept and call success callback

--- src/request.js
+++ src/request.js
@@ -5,13 +5,26 @@
     return;
   }
 
-  function get(url) {
+  function requestComplete(transport, options) {
+    if (transport.status == 200) {
+      options.success(transport);
+    }
+  }
+
+  function get(url, options) {
     if (typeof url != "string") {
       throw new TypeError("URL should be string");
     }
+
     var transport = ajax.create();
     transport.open("GET", url, true);
-    transport.onreadystatechange = function () {};
+
+    transport.onreadystatechange = function () {
+      if (transport.readyState == 4) {
+        requestComplete(transport, options);
+      }
+    };
+
     transport.send();
   }
 
