7a9451a1d9ff09a7b3312b577f0dd2c9c753a65f
Emil Wall <emil.wall@valtech.se>
2013-05-13 09:39:15 +0200

    Use named function and avoid full namespace

--- src/request.js
+++ src/request.js
@@ -1,5 +1,11 @@
-tddjs.namespace("ajax").get = function (url) {
-  if (typeof url != "string") {
-    throw new TypeError("URL should be string");
+(function () {
+  var ajax = tddjs.namespace("ajax");
+
+  function get(url) {
+    if (typeof url != "string") {
+      throw new TypeError("URL should be string");
+    }
   }
-};
+
+  ajax.get = get;
+}());
