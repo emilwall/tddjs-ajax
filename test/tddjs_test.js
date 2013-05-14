36dd95f60a486fce45f1a4e56027bc42041055fc
Emil Wall <emil.wall@valtech.se>
2013-05-14 10:10:39 +0200

    Increase testability of tddjs.isLocal

--- test/tddjs_test.js
+++ test/tddjs_test.js
@@ -12,6 +12,22 @@
   TestCase("IsLocalTest", {
     "test should define isLocal method": function () {
       assertFunction(tddjs.isLocal);
+    },
+
+    "test should return false when location is undefined": function () {
+      assertFalse(tddjs.isLocal(undefined));
+    },
+
+    "test should return false when location.protocol is http": function () {
+      assertFalse(tddjs.isLocal({
+        protocol: "http:"
+      }));
+    },
+
+    "test should return true when location.protocol is file": function () {
+      assertTrue(tddjs.isLocal({
+        protocol: "file:"
+      }));
     }
   });
 })();
